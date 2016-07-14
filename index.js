f9e6f6b7a3fb36be0c24c487e41690dfccb0b28b
Vaishnavi Iyer <email": null>
2016-07-14 17:37:13 +1200

    Setup FS for React/Redux Client

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/app'
+import App from './app/components/app'
 
 render(<App name='dinner-meetup' />, document.querySelector('main'))
 console.log('welcome to dinner-meetup')

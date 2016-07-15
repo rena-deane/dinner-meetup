390ff1e0a65ffddc78fe385445d19b7fa984e05a
Vaishnavi Iyer <email": null>
2016-07-15 15:22:20 +1200

    event List changes for filter

--- app/components/EventList.js
+++ app/components/EventList.js
@@ -4,15 +4,38 @@ import {Link} from 'react-router'
 
 class EventList extends Component {
   render () {
-  	const events = this.props.events.map( event => {
-  		return <EventCard key={event.get('id')} event={event}/>
-  	})
+
+    const type = this.params.filter
+
+    if(type === 'all'){
+      const events = this.props.events.map( event => {
+        return <EventCard key={event.get('id')} event={event}/>
+      })
+    }
+    else if (type === 'attending'){
+      const attending = this.props.events.filter( event =>{
+        return event.attend = true
+      })
+      const events = attending.map( event => {
+        return <EventCard key={event.get('id')} event={event}/>
+      })
+    }
+    else if (type === 'hosting'){
+      const attending = this.props.events.filter( event =>{
+        return event.host = true
+      })
+      const events = attending.map( event => {
+        return <EventCard key={event.get('id')} event={event}/>
+      })
+    }
+
     return (
-    	<div>
-    		<h1>This is EventList Page</h1>
-     	 	<div> { events } </div>
-     	</div>
+      <div>
+        <h1>This is EventList Page</h1>
+        <div> { events } </div>
+      </div>
     )
+    
   }
 }
 
